5e222124bbc2c01cc6ff7f6e4c4314aa8e8cb744
FShaaM <zinovevas135@gmail.com>
2026-09-19 18:20:53 +0300

    [TASK-1] I added an exit from the program under error‑catching conditions

--- aleksandr.zinovev/task-1/cmd/service/main.go
+++ aleksandr.zinovev/task-1/cmd/service/main.go
@@ -11,16 +11,19 @@ func main() {
 	_, err1 := fmt.Scan(&operand1)
 	if err1 != nil {
 		fmt.Println("Invalid first operand")
+		return
 	}
 
 	_, err2 := fmt.Scan(&operand2)
 	if err2 != nil {
 		fmt.Println("Invalid second operand")
+		return
 	}
 
 	_, err3 := fmt.Scan(&operation)
 	if err3 != nil {
 		fmt.Println("Invalid operation")
+		return
 	}
 
 	switch operation {
@@ -33,6 +36,7 @@ func main() {
 	case "/":
 		if operand2 == 0 {
 			fmt.Println("Division by zero")
+			return
 		}
 		fmt.Println(operand1 / operand2)
 	default:
