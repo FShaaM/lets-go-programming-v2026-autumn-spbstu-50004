4729bca6e6a0a40f9f511c34f57751152983c657
FShaaM <zinovevas135@gmail.com>
2026-09-19 17:49:48 +0300

    [TASK-1] I added variables and their reading

---
+++ aleksandr.zinovev/task-1/cmd/service/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var operand1, operand2 int
+	var operation string
+
+	_, err1 := fmt.Scan(&operand1)
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+	}
+
+	_, err2 := fmt.Scan(&operand2)
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+	}
+
+	_, err3 := fmt.Scan(&operation)
+	if err3 != nil {
+		fmt.Println("Invalid operation")
+	}
+}
