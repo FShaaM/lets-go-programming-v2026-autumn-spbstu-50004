aa7c4b443d0071a2efe68e40d0f885c70e2daa0b
FShaaM <zinovevas135@gmail.com>
2026-09-19 18:08:12 +0300

    [TASK-1] I added a check for division by zero

--- aleksandr.zinovev/task-1/cmd/service/main.go
+++ aleksandr.zinovev/task-1/cmd/service/main.go
@@ -31,6 +31,9 @@ func main() {
 	case "*":
 		fmt.Println(operand1 * operand2)
 	case "/":
+		if operand2 == 0 {
+			fmt.Println("Division by zero")
+		}
 		fmt.Println(operand1 / operand2)
 	default:
 		fmt.Println("Invalid operation")
